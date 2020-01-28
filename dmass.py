1b530181288ea7778ef81a73cbce1a75e8ea5dee
candiii <53330530+candiii@users.noreply.github.com>
2020-01-28 23:10:53 +0500

    Update dmass.py

--- dmass.py
+++ dmass.py
@@ -8,7 +8,7 @@ import colorsys
 import random
 import time
 
-client = commands.Bot(command_prefix = '+', case_insensitive=True)
+client = commands.Bot(command_prefix = '=', case_insensitive=True)
 Client = discord.client
 Clientdiscord = discord.Client()
 
@@ -17,7 +17,7 @@ async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
-    print('Created by niloj')
+    print('Created by candy')
 
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)     
@@ -44,4 +44,4 @@ async def send(ctx, *, content: str):
                 await client.say("DM can't Sent To : {} :x: ".format(member))
 
 
-client.run("YOUR TOKEN HERE")                
+client.run("NjcxNzc4NjQ5Nzk1NzIzMjc2.XjB4wg.n20CTLsPy4NWq674gSy6WPB31vY")                
